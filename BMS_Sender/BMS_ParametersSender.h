cd160077875543291f0e1dc5e636c55e9ad07e65
Keerthivasan-Murugesan <keerthim226@gmail.com>
2022-04-05 23:05:46 +0530

    Test update

--- BMS_Sender/BMS_ParametersSender.h
+++ BMS_Sender/BMS_ParametersSender.h
@@ -1,4 +1,4 @@
-#define NO_OF_READINGS 5
+#define NO_OF_READINGS 50
 
 void print(float temperature, float SOC, float CR);
 void ReadBMSParamsFromFile(float* Temperature, float* SOC, float* ChargeRate);
--- Test_BMSParms_Sender.cpp
+++ Test_BMSParms_Sender.cpp
@@ -3,7 +3,17 @@
 #include "test/catch.hpp"
 #include "BMS_ParametersSender.h"
 
-TEST_CASE("Tests to be developed") {
-	REQUIRE(1);
-    BMS_ParametersSender();
+TEST_CASE("Tests to check if data is correctly read from file") {
+  float Temperature[NO_OF_READINGS] = {0};
+  float StateOfCharge[NO_OF_READINGS] = {0};
+  float ChargeRate[NO_OF_READINGS] = {0};
+  ReadBMSParamsFromFile( Temperature,StateOfCharge,ChargeRate);
+  
+  float expectedoutput[3][3] = {{20,23,0.5}, {25,34,0.7},{45,67,0.05}};
+  for(int i=0;i<3;i++)
+  {
+    REQUIRE(Temperature[i] == expectedoutput[i][0]);
+    REQUIRE(StateOfCharge[i] == expectedoutput[i][1]);
+    REQUIRE(ChargeRate[i] == expectedoutput[i][2]);
+  }
 }
