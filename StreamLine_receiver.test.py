3ed4109904c1a3857582bf3dee28fe1e31bea2a1
Ranjeth-Sundaram1 <90901523+Ranjeth-Sundaram1@users.noreply.github.com>
2022-04-22 11:31:53 +0530

    Update StreamLine_receiver.test.py

--- StreamLine_receiver.test.py
+++ StreamLine_receiver.test.py
@@ -1,7 +1,7 @@
 import unittest
 import StreamLine_receiver
 
-sample_Value_from_sender = [
+Formatted_sample_Value_from_sender = [
             {"temp": 20, "SoC": 23, "ChargeRate": 0.5},
             {"temp": 25, "SoC": 34, "ChargeRate": 0.7},
             {"temp": 45, "SoC": 67, "ChargeRate": 0.05},
@@ -17,18 +17,18 @@ class TypewiseTest(unittest.TestCase):
         self.assertTrue(StreamLine_receiver.PrintStatisticsTOConsole("Average", "Charge rate", 0.276) == "Average value of Charge rate is 0.276")
     
     def test_PrintMovingAveragesintoConsole(self):
-        self.assertTrue(StreamLine_receiver.PrintMovingAveragesintoConsole(sample_Value_from_sender)==(29.2, 29.6, 0.276))
+        self.assertTrue(StreamLine_receiver.PrintMovingAveragesintoConsole(Formatted_sample_Value_from_sender)==(29.2, 29.6, 0.276))
 
     def test_PrintRangesOfParamsIntoConsole(self):
-        self.assertTrue(StreamLine_receiver.PrintRangesOfParamsIntoConsole(sample_Value_from_sender)==(15,45,10,67,0.05,0.7))
+        self.assertTrue(StreamLine_receiver.PrintRangesOfParamsIntoConsole(Formatted_sample_Value_from_sender)==(15,45,10,67,0.05,0.7))
 
     def test_FormatDataFromSender(self):
         self.assertTrue(StreamLine_receiver.FormatDataFromSender([" Temperature : 20 ", "State of Charge : 23" , "Charge Rate :0.5\n"])==[{'temp': 20.0, 'SoC': 23.0, 'ChargeRate': 0.5}])
 
     def test_CalculateMovingAverages(self):
-        self.assertTrue(StreamLine_receiver.CalculateMovingAverages(sample_Value_from_sender)==(29.2, 29.6, 0.276))
+        self.assertTrue(StreamLine_receiver.CalculateMovingAverages(Formatted_sample_Value_from_sender)==(29.2, 29.6, 0.276))
     
     def test_FindRangeValues(self):
-        self.assertTrue(StreamLine_receiver.FindRangeValues([one_reading['temp'] for one_reading in sample_Value_from_sender], [one_reading['SoC'] for one_reading in sample_Value_from_sender], [one_reading['ChargeRate'] for one_reading in sample_Value_from_sender])==(15,45,10,67,0.05,0.7))
+        self.assertTrue(StreamLine_receiver.FindRangeValues([one_reading['temp'] for one_reading in Formatted_sample_Value_from_sender], [one_reading['SoC'] for one_reading in Formatted_sample_Value_from_sender], [one_reading['ChargeRate'] for one_reading in Formatted_sample_Value_from_sender])==(15,45,10,67,0.05,0.7))
 
 unittest.main(),
